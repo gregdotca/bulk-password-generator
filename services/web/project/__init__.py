6621d0a1d78386c1539e7b43b003daf10d33b4c5
Greg Chetcuti <greg@chetcuti.com>
2025-03-09 14:48:27 -0700

    Update formatting

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python3
-
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask, render_template, request
 import random
 import string
+from flask import Flask, render_template, request
 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
 app = Flask(__name__, static_folder="assets")
 
 APP_TITLE = "Bulk Password Generator"
 
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
 @app.route("/")
 def home():
     return render_template("home.html", app_title=APP_TITLE)
@@ -65,9 +57,5 @@ def generate_password_list(number_of_passwords, password_length, exclude_special
     return "<BR>".join(password_list)
 
 
-# main driver function
 if __name__ == "__main__":
-
-    # run() method of Flask class runs the application
-    # on the local development server.
     app.run()
